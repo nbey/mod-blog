08436f034045d6664b9873c15f7fc84795367205
Nafis Bey <nafis@jarv.us>
2024-04-23 00:39:02 -0400

    style: post detail page

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -11,9 +11,26 @@ export default async function PostDetails({ params: { id }}) {
   const post = await loadPost(id)
 
   return (
-    <div className="m-4 p-4 w-full">
-      <h2 className="capitalize">{post.title}</h2>
-      <p className="my-4">{post.body}</p>
+    <div className="flex flex-col">
+      <div className="inline-flex items-center pt-4 pl-4">
+        <span className="sr-only">Prev Page</span>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <Link href={"/"} classes="pt-2 pl-2">
+          {'View All Posts'}
+        </Link>
+      </div>
+      <h1 className="capitalize p-4 text-4xl border-black border-solid border-b-2">
+        {post.title}
+      </h1>
+      <p
+        className="p-6 mt-8"
+      >{post.body}</p>
     </div>
   )
 }
