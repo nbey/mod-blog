2dd1b7d1d66744384e8ff81b4a35e38705cebab8
Nafis Bey <nafis@jarv.us>
2024-04-23 00:57:41 -0400

    fix: navbar style

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="sticky top-0 w-full px-24 backdrop-blur-2xl border-b-4 border-b-white">
-          <div className="z-10 w-full items-center">
-            <div className="flex w-full pb-6 pt-8 justify-center">
+        <nav className="sticky top-0 w-full px-24 backdrop-blur-2xl border-b-4 border-b-white bg-gradient-to-b from-zinc-200">
+          <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
+            <div className="flex w-full pb-6 pt-8 justify-center lg:justify-start">
               <a href="/" className="inline-flex">
                 <Image
                   src="/mod-logo.svg"
